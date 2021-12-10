135e1c21e675c1fdfc13a93f6f52fa86cf02af7a
puja <pujashm6@gmail.com>
2021-12-10 19:48:18 +0530

    solved nodejs routing for manually typing path

--- server/index.js
+++ server/index.js
@@ -16,6 +16,12 @@ app.get('/', (req, res) => {
     res.send('<h2>This is from index.js file</h2>');
 });
 
+// default route
+//If any of the previous routes do not match, this code will send back the index.html file from the build folder
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
 });
